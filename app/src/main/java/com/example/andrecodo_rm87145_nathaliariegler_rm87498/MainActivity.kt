8beea4b1e87078291acf19ebda5dc9cadb414102
Nathalia Riegler Schwertner Defavari <nathalia.schwertner@ibm.com>
2023-10-20 16:44:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/andrecodo_rm87145_nathaliariegler_rm87498/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.andrecodo_rm87145_nathaliariegler_rm87498
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
