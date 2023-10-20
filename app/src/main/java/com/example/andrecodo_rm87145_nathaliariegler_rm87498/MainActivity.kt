28f0a71fb28ad0b736d2f68e52ce892d45bbea3b
naschwertner <nathalia_13@outlook.com>
2023-10-20 18:05:16 -0300

    adiciona link endpoint

--- app/src/main/java/com/example/andrecodo_rm87145_nathaliariegler_rm87498/MainActivity.kt
+++ app/src/main/java/com/example/andrecodo_rm87145_nathaliariegler_rm87498/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.andrecodo_rm87145_nathaliariegler_rm87498
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.andrecodo_rm87145_nathaliariegler_rm87498.util.NetworkUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun getCurrencies(){
+        val retrofitClient = NetworkUtils.getRetrofitInstance("https://cdn.jsdelivr.net/")
+        val endpoint = retrofitClient.create(Endpoint::class.java)
     }
 }
--- app/src/main/java/com/example/andrecodo_rm87145_nathaliariegler_rm87498/util/NetworkUtils.kt
+++ app/src/main/java/com/example/andrecodo_rm87145_nathaliariegler_rm87498/util/NetworkUtils.kt
@@ -1,3 +1,5 @@
+package com.example.andrecodo_rm87145_nathaliariegler_rm87498.util
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
